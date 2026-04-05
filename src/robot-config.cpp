1524ff343f337788c35992f993afbbdf8b86f6de
Daniel Velasco <divelasco7@gmail.com>
2026-04-05 15:29:55 -0500

    absolutely perfect, almost no end turn

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -42,17 +42,17 @@ lemlib::OdomSensors sensors(&vertical_tracking_wheel, nullptr, &horizontal_track
 // lateral PID controller
 lemlib::ControllerSettings lateral_controller(7.5, // proportional gain (kP)
                                               0, // integral gain (kI)
-                                              32, // derivative gain (kD)
+                                              33, // derivative gain (kD)
                                               0, // anti windup
                                               0, // small error range, in inches
                                               0, // small error range timeout, in milliseconds
                                               0, // large error range, in inches
                                               0, // large error range timeout, in milliseconds
-                                              8 // maximum acceleration (slew)
+                                              7.5 // maximum acceleration (slew)
 );
 
 // angular PID controller
-lemlib::ControllerSettings angular_controller(6, 0, 44, 0, 1, 85, 3, 300, 0);
+lemlib::ControllerSettings angular_controller(5.7, 0, 42, 0, 1, 85, 3, 300, 0);
 
 // create the chassis
 lemlib::Chassis chassis(drivetrain, lateral_controller, angular_controller, sensors);
