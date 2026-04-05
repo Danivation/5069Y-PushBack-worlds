6b8252d02084a70e2a1b06a961c055d7aac93283
Daniel Velasco <divelasco7@gmail.com>
2026-04-04 21:18:48 -0500

    HOLY

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -67,9 +67,9 @@ lemlib::ControllerSettings lateral_controller(10, // proportional gain (kP)
 );
 
 // angular PID controller
-lemlib::ControllerSettings angular_controller(3.2, // proportional gain (kP)
+lemlib::ControllerSettings angular_controller(2.9, // proportional gain (kP)
                                               0, // integral gain (kI)
-                                              22.3, // derivative gain (kD)
+                                              18, // derivative gain (kD)
                                               0, // anti windup
                                               0, // small error range, in degrees
                                               0, // small error range timeout, in milliseconds
