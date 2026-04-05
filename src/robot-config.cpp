312b67c5d6cee380745fa960d753bc01745b78ef
Daniel Velasco <divelasco7@gmail.com>
2026-04-04 21:26:17 -0500

    under but back

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -69,8 +69,8 @@ lemlib::ControllerSettings lateral_controller(10, // proportional gain (kP)
 // angular PID controller
 lemlib::ControllerSettings angular_controller(2.9, // proportional gain (kP)
                                               0, // integral gain (kI)
-                                              18, // derivative gain (kD)
-                                              0, // anti windup
+                                              20, // derivative gain (kD)
+                                              4, // anti windup
                                               0, // small error range, in degrees
                                               0, // small error range timeout, in milliseconds
                                               0, // large error range, in degrees
